06eea6845114ad6075a9e4b7f6479bfb2b0e9d84
MOZGIII <mike-n@narod.ru>
2023-11-04 01:30:48 -0300

    Install chrome into CI as well

--- .github/scripts/build_env/ubuntu.sh
+++ .github/scripts/build_env/ubuntu.sh
@@ -7,8 +7,10 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 curl -sSL "https://github.com/rustwasm/wasm-bindgen/releases/download/0.2.87/wasm-bindgen-0.2.87-x86_64-unknown-linux-musl.tar.gz" |
   sudo tar -xzvf - -C /usr/local/bin --strip-components=1
 
-# Install chromedriver.
-npx @puppeteer/browsers install chromedriver@118.0.5993.32
+# Install chromedriver and chrome.
+CHROME_VERSION="118.0.5993.32"
+npx @puppeteer/browsers install "chromedriver@${CHROME_VERSION}"
+npx @puppeteer/browsers install "chrome@${CHROME_VERSION}"
 
 # Set CHROMEDRIVER env var.
 CHROMEDRIVERS=(chromedriver/*/chromedriver-*/chromedriver)
