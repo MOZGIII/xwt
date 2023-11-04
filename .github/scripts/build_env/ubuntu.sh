8ee1bdf6ecd3f4ca1a3dc39412ee0c66c6c26a56
MOZGIII <mike-n@narod.ru>
2023-11-04 00:44:09 -0300

    Update wasm-bindgen-test-runner

--- .github/scripts/build_env/ubuntu.sh
+++ .github/scripts/build_env/ubuntu.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 # Install wasm-bindgen-test-runner.
-curl -sSL "https://github.com/rustwasm/wasm-bindgen/releases/download/0.2.87/wasm-bindgen-0.2.87-x86_64-unknown-linux-musl.tar.gz" |
+curl -sSL "https://github.com/rustwasm/wasm-bindgen/releases/download/0.2.88/wasm-bindgen-0.2.88-x86_64-unknown-linux-musl.tar.gz" |
   sudo tar -xzvf - -C /usr/local/bin --strip-components=1
 
 # Install chromedriver and chrome.
