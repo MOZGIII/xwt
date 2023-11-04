4a11b287ad0f41f7a24dbab974069d50df0ac9ff
MOZGIII <mike-n@narod.ru>
2023-11-04 00:11:57 -0300

    Rename xwebtransport to xwt

--- crates/xwebtransport/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(target_family = "wasm")]
-pub use xwebtransport_web_sys as web_sys;
-#[cfg(not(target_family = "wasm"))]
-pub use xwebtransport_wtransport as wtransport;
-
-#[cfg(target_family = "wasm")]
-pub use self::web_sys as current;
-#[cfg(not(target_family = "wasm"))]
-pub use self::wtransport as current;
