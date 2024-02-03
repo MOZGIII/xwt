81420c69186b586b0fed7920db8072f4ba601848
MOZGIII <mike-n@narod.ru>
2024-02-03 03:41:07 -0300

    Add connection drop test

--- crates/xwt-web-sys/tests/test.rs
+++ crates/xwt-web-sys/tests/test.rs
@@ -1,14 +1,14 @@
 #![cfg(target_family = "wasm")]
 
-use wasm_bindgen::prelude::wasm_bindgen;
 use wasm_bindgen_test::*;
 
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
-#[wasm_bindgen(start)]
-pub fn start() -> Result<(), wasm_bindgen::JsValue> {
-    tracing_wasm::set_as_global_default();
-    Ok(())
+fn setup() {
+    static INIT: std::sync::Once = std::sync::Once::new();
+    INIT.call_once(|| {
+        tracing_wasm::set_as_global_default();
+    });
 }
 
 fn test_endpoint() -> xwt_web_sys::Endpoint {
@@ -33,6 +33,8 @@ fn test_endpoint() -> xwt_web_sys::Endpoint {
 
 #[wasm_bindgen_test]
 async fn echo_streams() {
+    setup();
+
     let endpoint = test_endpoint();
 
     xwt_tests::tests::echo(endpoint, xwt_tests::consts::ECHO_SERVER_URL)
@@ -42,6 +44,8 @@ async fn echo_streams() {
 
 #[wasm_bindgen_test]
 async fn echo_datagrams() {
+    setup();
+
     let endpoint = test_endpoint();
 
     xwt_tests::tests::echo_datagrams(endpoint, xwt_tests::consts::ECHO_SERVER_URL)
@@ -51,9 +55,37 @@ async fn echo_datagrams() {
 
 #[wasm_bindgen_test]
 async fn read_small_buf() {
+    setup();
+
     let endpoint = test_endpoint();
 
     xwt_tests::tests::read_small_buf::run(endpoint, xwt_tests::consts::ECHO_SERVER_URL)
         .await
         .unwrap();
+}
+
+#[wasm_bindgen_test]
+async fn connection_drop() {
+    setup();
+
+    let endpoint = test_endpoint();
+
+    xwt_tests::tests::connection_drop::run(endpoint, xwt_tests::consts::ECHO_SERVER_URL, |error| {
+        let known_bad_errors = ["Connection lost."];
+        let known_good_errors = ["WebTransportError: The session is closed."];
+        let actual_error = error.to_string();
+
+        let is_bad_error = known_bad_errors
+            .into_iter()
+            .any(|known_bad_error| actual_error.contains(known_bad_error));
+        if is_bad_error {
+            return false;
+        }
+
+        known_good_errors
+            .into_iter()
+            .any(|is_good_error| actual_error.contains(is_good_error))
+    })
+    .await
+    .unwrap();
 }
