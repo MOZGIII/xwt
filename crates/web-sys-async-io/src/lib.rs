19cac9c8709eea095424c933373b2e19e4261e4c
MOZGIII <mike-n@narod.ru>
2024-02-04 15:15:47 -0300

    Fix the error conversion at web-sys-async-io

--- crates/web-sys-async-io/src/lib.rs
+++ crates/web-sys-async-io/src/lib.rs
@@ -16,9 +16,7 @@ pub use self::reader::Reader;
 pub use self::writer::Writer;
 
 fn js_value_to_io_error(error: wasm_bindgen::JsValue) -> std::io::Error {
-    let err: String = js_sys::JSON::stringify(&error)
-        .map(Into::into)
-        .unwrap_or_default();
+    let err: String = format!("{error:?}");
     std::io::Error::new(std::io::ErrorKind::Other, err)
 }
 
