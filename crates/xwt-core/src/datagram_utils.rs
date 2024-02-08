510430aed73ac9601b32d0f7582209be3f426bbe
MOZGIII <mike-n@narod.ru>
2024-02-07 23:08:55 -0300

    Add a test

--- crates/xwt-core/src/datagram_utils.rs
+++ crates/xwt-core/src/datagram_utils.rs
@@ -1,9 +1,9 @@
 use crate::datagram;
 
-pub type ReceiveErrorFor<T> = <T as datagram::Receive>::Error;
+pub type SendErrorFor<T> = <T as datagram::Send>::Error;
 
-pub type ReceiveIntoErrorFor<T> = <T as datagram::ReceiveInto>::Error;
+pub type ReceiveErrorFor<T> = <T as datagram::Receive>::Error;
 
 pub type ReceiveDatagramFor<T> = <T as datagram::Receive>::Datagram;
 
-pub type SendErrorFor<T> = <T as datagram::Send>::Error;
+pub type ReceiveIntoErrorFor<T> = <T as datagram::ReceiveInto>::Error;
