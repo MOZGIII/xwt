d79d274b03adba38cfd0d7483e88cdd07d02ebc9
MOZGIII <mike-n@narod.ru>
2024-04-06 18:29:11 -0300

    Tweak crates std requirement

--- crates/web-webtransport-sys/src/lib.rs
+++ crates/web-webtransport-sys/src/lib.rs
@@ -4,6 +4,8 @@
 //!
 //! [idl]: https://w3c.github.io/webtransport/#idl-index
 
+#![no_std]
+
 /// Generated bindings.
 #[allow(missing_docs, clippy::missing_docs_in_private_items, unsafe_code)]
 mod gen;
