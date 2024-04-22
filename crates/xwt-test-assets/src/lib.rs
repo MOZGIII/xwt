7e574c5b572acd038b67b9600537e7431b7f9764
MOZGIII <mike-n@narod.ru>
2024-04-22 01:36:14 -0300

    Use key in pkcs8 format instead of sec1
    Also, looks like sec1 serialization is kind of broken there...

--- crates/xwt-test-assets/src/lib.rs
+++ crates/xwt-test-assets/src/lib.rs
@@ -1,7 +1,7 @@
 //! The xwt test assets.
 
-/// The certificate in the DER format.
+/// The certificate in the DER X.509 format.
 pub const CERT: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/target/cert.der"));
 
-/// The private key in the DER format.
-pub const KEY: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/target/key-sec1.der"));
+/// The private key in the DER PKCS 8 format.
+pub const KEY: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/target/key-pkcs8.der"));
