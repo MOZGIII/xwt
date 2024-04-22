e206a8d3e74270d717c36a2e76b64cd1b7d0c445
MOZGIII <mike-n@narod.ru>
2024-04-22 02:26:10 -0300

    Refactor test assets building (#148)
    * Refactor test assets building
    
    * Use key in pkcs8 format instead of sec1
    
    Also, looks like sec1 serialization is kind of broken there...

--- crates/xwt-test-assets/build.rs
+++ crates/xwt-test-assets/build.rs
@@ -2,7 +2,7 @@
 
 fn main() {
     xwt_test_assets_build::save(
-        xwt_test_assets_build::generate(),
         xwt_test_assets_build::env_dir("CARGO_MANIFEST_DIR").join("target"),
+        xwt_test_assets_build::generate(),
     );
 }
