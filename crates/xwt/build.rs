9efa42c2d53cef91623d00018b13b138b13d70eb
MOZGIII <mike-n@narod.ru>
2024-04-21 22:54:01 -0300

    Deprecate the `xwt` crate

---
+++ crates/xwt/build.rs
@@ -0,0 +1,20 @@
+//! A build script to print the deprecation warning.
+
+/// The deprecation message to print during build.
+const DEPRECATION_MESSAGE: &str = "\
+This crate provides a simple mapping to one of the drivers depending on \
+the current build traget (native vs wasm). For better portability, \
+the `xwt-core` crate should be used instead, and the drivers should \
+be hand-picked for each of the targets instead of using  a crate like this one. \
+We will be removing the `xwt` crate soon, and you are  free to import \
+`xwt-web-sys` and `xwt-wtransport` manually and put them under \
+a `cfg_if!` macro yourself if you prefer this mode of operation; they key \
+difference there is you maintain explicit control over the your driver \
+dependencies rather than us having to tie them together for you - and this how \
+it should be. \
+See the examples for more info on how we intend for xwt to be used.\
+";
+
+fn main() {
+    println!("cargo:warning={DEPRECATION_MESSAGE}");
+}
--- crates/xwt/src/lib.rs
+++ crates/xwt/src/lib.rs
@@ -1,11 +1,19 @@
 //! The ╳-platform WebTransport implementation.
+//!
+//! DEPRECATED: use [`xwt-core`] and manually picked drvers instead.
+//!
+//! See the examples in the repo on how `xwt` is intended to be used.
 
-#[cfg(target_family = "wasm")]
-pub use xwt_web_sys as web_sys;
-#[cfg(not(target_family = "wasm"))]
-pub use xwt_wtransport as wtransport;
+cfg_if::cfg_if! {
+    if #[cfg(target_family = "wasm")] {
 
-#[cfg(target_family = "wasm")]
-pub use self::web_sys as current;
-#[cfg(not(target_family = "wasm"))]
-pub use self::wtransport as current;
+        pub use xwt_web_sys as web_sys;
+        pub use self::web_sys as current;
+
+    } else {
+
+        pub use xwt_wtransport as wtransport;
+        pub use self::wtransport as current;
+
+    }
+}
