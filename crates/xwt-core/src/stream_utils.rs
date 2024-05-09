5bce3aca4c660f5522d13a0bc268908162d3a251
MOZGIII <mike-n@narod.ru>
2024-05-09 02:23:54 -0300

    Correct prelude and steram utils for new traits

--- crates/xwt-core/src/stream_utils.rs
+++ crates/xwt-core/src/stream_utils.rs
@@ -13,3 +13,24 @@ pub type ReadChunkErrorFor<T, Data> = <T as stream::ReadChunk<Data>>::Error;
 
 /// A shortcut for the error type for a given [`stream::WriteChunk] type.
 pub type WriteChunkErrorFor<T, Data> = <T as stream::WriteChunk<Data>>::Error;
+
+/// A shortcut for the error type for a given [`stream::ReadAbort`] type.
+pub type ReadAbortErrorFor<T> = <T as stream::ReadAbort>::Error;
+
+/// A shortcut for the error code type for a given [`stream::ReadAbort`] type.
+pub type ReadAbortErrorCodeFor<T> = <T as stream::ReadAbort>::ErrorCode;
+
+/// A shortcut for the error type for a given [`stream::WriteAbort`] type.
+pub type WriteAbortErrorFor<T> = <T as stream::WriteAbort>::Error;
+
+/// A shortcut for the error code type for a given [`stream::WriteAbort`] type.
+pub type WriteAbortErrorCodeFor<T> = <T as stream::WriteAbort>::ErrorCode;
+
+/// A shortcut for the error type for a given [`stream::WriteAborted`] type.
+pub type WriteAbortedErrorFor<T> = <T as stream::WriteAborted>::Error;
+
+/// A shortcut for the error code type for a given [`stream::WriteAborted`] type.
+pub type WriteAbortedErrorCodeFor<T> = <T as stream::WriteAborted>::ErrorCode;
+
+/// A shortcut for the error type for a given [`stream::Finish`] type.
+pub type FinishErrorFor<T> = <T as stream::Finish>::Error;
