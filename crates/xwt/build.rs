4a48b46d2cb71c1140584deff828745736373822
MOZGIII <mike-n@narod.ru>
2024-09-13 17:45:47 -0300

    Drop the xwt root crate re-exports and make it completely empty
    This will make way for the further work to be placed there.

--- crates/xwt/build.rs
+++ crates/xwt/build.rs
@@ -5,14 +5,14 @@ const DEPRECATION_MESSAGE: &str = "\
 This crate provides a simple mapping to one of the drivers depending on \
 the current build target (native vs wasm). For better portability, \
 the `xwt-core` crate should be used instead, and the drivers should \
-be hand-picked for each of the targets instead of using  a crate like this one. \
-We will be removing the `xwt` crate soon, and you are  free to import \
+be hand-picked for each of the targets instead of using a crate like this one. \
+We will be removing the `xwt` crate soon, and you are free to import \
 `xwt-web-sys` and `xwt-wtransport` manually and put them under \
 a `cfg_if!` macro yourself if you prefer this mode of operation; they key \
 difference there is you maintain explicit control over the your driver \
 dependencies rather than us having to tie them together for you - and this how \
 it should be. \
-See the examples for more info on how we intend for xwt to be used.\
+See the examples on Github for more info on how we intend for xwt to be used.\
 ";
 
 fn main() {
--- crates/xwt/src/lib.rs
+++ crates/xwt/src/lib.rs
@@ -3,17 +3,3 @@
 //! DEPRECATED: use [`xwt-core`] and manually picked drvers instead.
 //!
 //! See the examples in the repo on how `xwt` is intended to be used.
-
-cfg_if::cfg_if! {
-    if #[cfg(target_family = "wasm")] {
-
-        pub use xwt_web_sys as web_sys;
-        pub use self::web_sys as current;
-
-    } else {
-
-        pub use xwt_wtransport as wtransport;
-        pub use self::wtransport as current;
-
-    }
-}
