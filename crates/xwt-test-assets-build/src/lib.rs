ffbe8492e7fa82d35f024e54c19e86b4bd7d3093
MOZGIII <mike-n@narod.ru>
2025-08-21 16:43:01 +0400

    Check for the test assets expiration

--- crates/xwt-test-assets-build/src/lib.rs
+++ crates/xwt-test-assets-build/src/lib.rs
@@ -1,7 +1,7 @@
 //! The build dependency for the xwt test assets.
 
 #![cfg(not(target_family = "wasm"))]
-#![allow(missing_docs)]
+#![allow(missing_docs, clippy::missing_docs_in_private_items)]
 
 use std::{
     io::ErrorKind,
@@ -16,6 +16,8 @@ use p256::pkcs8::{
 
 pub type AnyBytes = Box<dyn AsRef<[u8]>>;
 
+pub const GENERATED_AT_FILE_NAME: &str = "generated-at.txt";
+
 pub fn generate() -> Vec<(&'static str, AnyBytes)> {
     let params =
         xwt_cert_gen::Params::new("xwt-test-certificate", &["localhost", "127.0.0.1", "::1"]);
@@ -29,6 +31,11 @@ pub fn generate() -> Vec<(&'static str, AnyBytes)> {
     let key: p256::SecretKey = key.into();
 
     let line_ending = p256::pkcs8::LineEnding::LF;
+
+    let generated_at = time::UtcDateTime::now();
+    let generated_at_string = generated_at
+        .format(&time::format_description::well_known::Rfc3339)
+        .unwrap();
 
     vec![
         ("cert.der", Box::new(cert.to_der().unwrap())),
@@ -46,6 +53,7 @@ pub fn generate() -> Vec<(&'static str, AnyBytes)> {
             "key-pkcs8.pem",
             Box::new(key.to_pkcs8_pem(line_ending).unwrap()),
         ),
+        (GENERATED_AT_FILE_NAME, Box::new(generated_at_string)),
     ]
 }
 
@@ -66,6 +74,8 @@ pub fn save(dir: impl AsRef<Path>, files: Vec<(&'static str, AnyBytes)>) -> Save
 
     std::fs::create_dir_all(dir).unwrap();
 
+    let expiration_check_result = check_for_expired_generation(dir);
+
     let open = |file| {
         std::fs::OpenOptions::new()
             .create_new(true)
@@ -73,27 +83,84 @@ pub fn save(dir: impl AsRef<Path>, files: Vec<(&'static str, AnyBytes)>) -> Save
             .open(file)
     };
 
-    let results: Vec<_> = files
-        .iter()
-        .map(|(file_name, _)| open(dir.join(file_name)))
-        .collect();
+    let paths = files.iter().map(|(file_name, _)| dir.join(file_name));
 
-    // *Before* writing the files, check if all the files are in valid state -
-    // as maybe we don't need to do anything.
-    if results
-        .iter()
-        .all(|result| matches!(result, Err(err) if err.kind() == ErrorKind::AlreadyExists))
-    {
-        // We should check if the files are expired here and force-replace
-        // them if needed - but I'll skip this for now.
-        return SaveOutcome::AllFilesWerePresent;
+    for path in paths.clone() {
+        println!("cargo::rerun-if-changed={}", path.display());
     }
 
-    for (result, (_, data)) in results.into_iter().zip(files.into_iter()) {
-        let mut file = result.unwrap();
+    let results: Vec<_> = paths.map(open).collect();
+
+    let all_files_exist = results
+        .iter()
+        .all(|result| matches!(result, Err(err) if err.kind() == ErrorKind::AlreadyExists));
+
+    // *Before* writing the files, check if all the files are present -
+    // as maybe we don't need to do anything.
+    // But only when the stuff we have is not expired...
+    if let ExpirationCheckResult::NotStale = expiration_check_result {
+        if all_files_exist {
+            // We should check if the files are expired here and force-replace
+            // them if needed - but I'll skip this for now.
+            return SaveOutcome::AllFilesWerePresent;
+        }
+    }
+
+    eprintln!("Saving new xwt test assets");
+
+    for (result, (file_name, data)) in results.into_iter().zip(files.into_iter()) {
+        let mut file = match result {
+            Ok(file) => file,
+            Err(err) if err.kind() == ErrorKind::AlreadyExists => std::fs::OpenOptions::new()
+                .write(true)
+                .truncate(true)
+                .open(dir.join(file_name))
+                .unwrap(),
+            other => other.unwrap(),
+        };
+
         file.write_all((*data).as_ref()).unwrap();
         file.flush().unwrap();
     }
 
     SaveOutcome::AllFilesWereWritten
+}
+
+enum ExpirationCheckResult {
+    ShouldRegenerate,
+    NotStale,
+}
+
+fn check_for_expired_generation(dir: &Path) -> ExpirationCheckResult {
+    let generated_at_string = match std::fs::read_to_string(dir.join(GENERATED_AT_FILE_NAME)) {
+        Ok(val) => val,
+        Err(err) if err.kind() == ErrorKind::NotFound => {
+            return ExpirationCheckResult::ShouldRegenerate
+        }
+        other => other.unwrap(),
+    };
+
+    if generated_at_string.is_empty() {
+        eprintln!("The xwt test assets are not present and should be created");
+        return ExpirationCheckResult::ShouldRegenerate;
+    }
+
+    let generated_at = time::UtcDateTime::parse(
+        generated_at_string.trim_ascii(),
+        &time::format_description::well_known::Rfc3339,
+    )
+    .unwrap();
+
+    let now = time::UtcDateTime::now();
+
+    let generated_expires_at =
+        generated_at.saturating_add(time::Duration::weeks(2) - time::Duration::days(1));
+
+    if now > generated_expires_at {
+        eprintln!("The xwt test assets are expired and should be updated");
+        return ExpirationCheckResult::ShouldRegenerate;
+    }
+
+    eprintln!("The xwt test assets are up-to-date");
+    ExpirationCheckResult::NotStale
 }
